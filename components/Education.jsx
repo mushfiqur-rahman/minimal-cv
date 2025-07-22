c7dae06e1da7d8d4bfafbda924c5b38e394d9a29
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-07-22 09:08:25 +0600

    project update

--- components/Education.jsx
+++ components/Education.jsx
@@ -4,24 +4,24 @@ const Education = () => {
   return (
     <>
       <div className="py-4">
-        <section class="flex min-h-0 flex-col gap-y-3">
-          <h2 class="text-xl font-bold" id="education-section">
+        <section className="flex min-h-0 flex-col gap-y-3">
+          <h2 className="text-xl font-bold" id="education-section">
             Education
           </h2>
           <div
-            class="space-y-4"
+            className="space-y-4"
             role="feed"
             aria-labelledby="education-section"
           >
             <article role="article">
-              <div class="rounded-lg bg-card text-card-foreground">
-                <div class="flex flex-col space-y-1.5">
-                  <div class="flex items-center justify-between gap-x-2 text-base">
-                    <h3 class="font-semibold leading-none">
+              <div className="rounded-lg bg-card text-card-foreground">
+                <div className="flex flex-col space-y-1.5">
+                  <div className="flex items-center justify-between gap-x-2 text-base">
+                    <h3 className="font-semibold leading-none">
                       ILACS(National University)
                     </h3>
                     <div
-                      class="text-sm tabular-nums"
+                      className="text-sm tabular-nums"
                       aria-label="Period: 2012-2016"
                     >
                       2012-2016
@@ -29,7 +29,7 @@ const Education = () => {
                   </div>
                 </div>
                 <div
-                  class="text-pretty font-mono text-sm mt-2 text-foreground/80 print:text-[12px]"
+                  className="text-pretty font-mono text-sm mt-2 text-foreground/80 print:text-[12px]"
                   aria-labelledby="education-National-University"
                 >
                   Bachelor of Science in Computer Science & Engineering
