86b3a4ba79c056d18ca59d17fd0cd6e8cb9389ab
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-08-11 15:21:51 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="content-language" content="en-US" />
         <meta name="robots" content="follow, index" />
-        <meta name="google-site-verification" content="" />
-        <meta name="yandex-verification" content="" />
+        <meta
+          name="google-site-verification"
+          content="UviZELNJ6Xu6G42kgGmKtVzRXpoc61MYCLpWmoV1uzA"
+        />
+        <meta name="yandex-verification" content="f603ed602ebfae14" />
 
-        <meta name="facebook-domain-verification" content="" />
+        <meta
+          name="facebook-domain-verification"
+          content="0l6xk00ijn0tbpfyr2d5n7gqgn4x8f"
+        />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="" key="desc" />
+        <meta
+          name="description"
+          content="Experienced IT Support Engineer helping remotly US, Australia, United Kingdom businesses with Microsoft 365, Google Workspace, DNS, email, and SEO-friendly websites."
+          key="desc"
+        />
         <meta content="#ffffff" name="theme-color" />
-        <meta content="" name="position" />
+        <meta
+          content="Experienced IT Support Engineer helping US, Australia, United Kingdom businesses with Microsoft 365, Google Workspace, DNS, email, and SEO-friendly websites."
+          name="position"
+        />
         <meta name="distribution" content="Global" />
         <meta name="coverage" content="Worldwide" />
         <meta name="rating" content="General" />
@@ -34,24 +47,51 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta name="msvalidate.01" content="3EA8D19855F5F7D04AEABD2C2A95FA33" />
         <meta property="og:site_name" content="Mushfiqur Rahman" />
-        <meta property="og:description" content="" />
-        <meta property="og:title" content="" />
-        <meta property="og:image" content="" />
-        <meta property="fb:app_id" content="" />
+        <meta
+          property="og:description"
+          content="Experienced IT Support Engineer helping US, Australia, United Kingdom businesses with Microsoft 365, Google Workspace, DNS, email, and SEO-friendly websites."
+        />
+        <meta
+          property="og:title"
+          content="Mushfiqur Rahman  and an IT support engineer"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/depyaowh3/image/upload/v1743254614/mushfiq-wide_image_q4up1c.png"
+        />
+        <meta property="fb:app_id" content="353614617613762" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="628" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="" />
-        <meta name="keywords" content="" />
-
-        <link href="https://minimal-cv-theta.vercel.app" rel="canonical" />
-        <meta content="https://minimal-cv-theta.vercel.app" property="og:url" />
-        <meta content="" name="author" />
-        <meta content="" name="application-name" />
-        <title>MUSHFIQ - Next JS Minimal CV</title>
+        <meta name="twitter:site" content="@MushfiqFeed" />
+        <meta
+          name="twitter:title"
+          content="Mushfiqur Rahman and an IT support engineer"
+        />
+        <meta
+          name="twitter:description"
+          content="Experienced IT Support Engineer  helping US, Australia, United Kingdom businesses with Microsoft 365, Google Workspace, DNS, email, and SEO-friendly websites."
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/depyaowh3/image/upload/v1743254614/mushfiq-wide_image_q4up1c.png"
+        />
+        <meta
+          name="keywords"
+          content="Mushfiqur Rahman, experienced freelancer, IT Expert,Remote Services,Remote IT Service, IT support engineer, Start up Software Engineer, Software Engineer, Next js Software Engineer, python django Software Engineer, Google Workspace administration, Microsoft Office 365 administration, email deliverability, WordPress website bug-free transfer, DNS management (GoDaddy, Cloudflare, Namecheap), website & DNS issue fixes (CNAME, A records, IP, redirects), spam or junk issue fix, cold mail setup, email migration, and IT consultancy"
+        />
+        <meta
+          name="impact-site-verification"
+          value="0074120f-c25b-4fa8-8c30-58ad69f0ad07"
+        />
+        <link href="https://mushfiq.xyz" rel="canonical" />
+        <meta content="https://mushfiq.xyz" property="og:url" />
+        <meta
+          content="Mushfiqur Rahman is an IT support engineer"
+          name="author"
+        />
+        <meta content="Mushfiqur Rahman Portfolio" name="application-name" />
+        <title>MUSHFIQ - IT Support Engineer | Web Developer</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <main
