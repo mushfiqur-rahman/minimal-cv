dd94bdfec4e2eaa3df97b7466ebff74445ab9478
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-08-11 15:34:28 +0600

    seo improved

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import PinterestTag from "@/components/PinterestTag";
+import Progress from "@/components/Progress";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
+import SiteSchema from "@/components/Schema";
 
 const manrope = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -10,6 +13,10 @@ const manrope = Montserrat({
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Progress />
+      <PinterestTag />
+      <SiteSchema />
+
       <main className={manrope.className}>
         <Component {...pageProps} />
       </main>
